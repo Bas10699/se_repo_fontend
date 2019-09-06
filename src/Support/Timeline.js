370d59c120c3d649c33465a57989e3666b5e5a34
bas <bas10699@gmail.com>
2019-09-06 13:58:14 +0700

    Auto stash before merge of "bas" and "origin/master"

--- src/Support/Timeline.js
+++ src/Support/Timeline.js
@@ -52,10 +52,17 @@ class Timeline extends Component {
                     <div className="col-2"></div>
                     <div className="col-8">
                         <ul className="Progressbar">
+<<<<<<< Updated upstream
                             <NavLink to="#0"><li activeClassName="Active" className="Standat" >ส่งใบสั่งซ์้อ</li></NavLink>
                             <NavLink to="#1"><li activeClassName="Active" className="Standat" >ยืนยันคำสั่งซื้อ</li></NavLink>
                             <NavLink to="#2"><li activeClassName="Active" className="Standat" >ใบเเจ้งหนี้</li></NavLink>
                             <NavLink to="#3"><li activeClassName="Active" className="Standat" >ใบเสร็จ</li></NavLink>
+=======
+                            <NavLink to="#0">{this.props.status ==1 ? <li className="Standat" >ส่งใบสั่งซ์้อ</li> : <li className="Standat Active" >ส่งใบสั่งซ์้อ</li> }</NavLink>
+                            <NavLink to="#1"><li activeClassName="Active" className="Standat">ยืนยันคำสั่งซื้อ</li></NavLink>
+                            <NavLink to="#2"><li activeClassName="Active" className="Standat">ใบเเจ้งหนี้</li></NavLink>
+                            <NavLink to="#3"><li activeClassName="Active" className="Standat">ใบเสร็จ</li></NavLink>
+>>>>>>> Stashed changes
                         </ul>
 
                         {/* {this.state.status < 5 ? this.render_Step(this.state.status) : this.setState({ status: 5 })} */}
