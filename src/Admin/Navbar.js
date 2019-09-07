54079c581dbbea46751d041edc908286c9100894
PEDPIIP <pedpii69@gmail.com>
2019-09-07 19:23:40 +0700

    7/9/62 17.23

--- src/Admin/Navbar.js
+++ src/Admin/Navbar.js
@@ -105,6 +105,7 @@ class Navbar extends Component {
                                 <li><NavLink exact to="/" activeClassName="Active" className="NavbarText">หน้าเเรก</NavLink></li>
                                 <li><NavLink exact to="/Product" activeClassName="Active" className="NavbarText">สินค้า</NavLink></li>
                                 <li><NavLink exact to="/M_Order" activeClassName="Active" className="NavbarText">คำสั่งซื้อผู้ประกอบการ</NavLink></li>
+                                <li><NavLink exact to="#userAll" activeClassName="Active" className="NavbarText">ผู้ใช้งาน</NavLink></li>
                                 <NavLink to="/Signin" activeClassName="Active" className="NavbarRight" onClick={this.logOut.bind(this)} >ออกจากระบบ</NavLink>
                                 <NavLink exact to="/User" activeClassName="Active" className="NavbarRight" >{this.state.get_user.username}</NavLink>
                             </ul>
