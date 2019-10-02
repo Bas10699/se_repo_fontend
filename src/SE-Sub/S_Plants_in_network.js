76e78e99730ecb75e94c43c7fca63ae7100350d2
PEDPIIP <pedpii69@gmail.com>
2019-10-02 11:32:15 +0700

    2/10/62 11.32

--- src/SE-Sub/S_Plants_in_network.js
+++ src/SE-Sub/S_Plants_in_network.js
@@ -3,44 +3,57 @@ import React, { Component } from 'react'
 import { get, post, ip } from '../Support/Service'
 import { user_token } from '../Support/Constance'
 
-class S_Plants_in_network extends Component{
-    constructor(props){
+class S_Plants_in_network extends Component {
+    constructor(props) {
         super(props)
-        this.state={
-            plants:[]
+        this.state = {
+            plants: []
 
         }
     }
-    componentWillMount(){
+    componentWillMount() {
         this.get_plant()
     }
-    get_plant = async () =>{
-        try{
-            await get('neo_firm/get_plant_in_network',user_token).then((result)=>{
-                if(result.success){
+    get_plant = async () => {
+        try {
+            await get('neo_firm/get_plant_in_network', user_token).then((result) => {
+                if (result.success) {
                     this.setState({
-                        plants:result.result[0].plant
+                        plants: result.result[0].plant
                     })
                     console.log(result.result[0])
                 }
-                else{
+                else {
                     alert(result.error_message)
                 }
             })
 
-        }catch(error){
-            alert('get_plant: '+error)
-            
+        } catch (error) {
+            alert('get_plant: ' + error)
+
         }
     }
-    render(){
-        return(
+    render() {
+        return (
             <div className="App">
-                {this.state.plants.map((element)=>{
-                    return(
-                        <div>{element.name}</div>
-                    )
-                })}
+                <div className="Row">
+                    <div className="col-12">
+                        <h2 style={{ textAlign: "center" }}>ผลผลิตที่ส่งมอบได้</h2>
+                    </div>
+                </div>
+
+                <div className="Row">
+                    <div className="col-1"></div>
+                    <div className="col-10">
+                        {this.state.plants.map((element) => {
+                            return (
+                                <div>{element.name}</div>
+                            )
+                        })}
+                    </div>
+                    <div className="col-1"></div>
+                </div>
+
             </div>
         )
     }
