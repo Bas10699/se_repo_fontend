d77ef31eec68486de52f020e1a132c8418b8feb5
bas <bas10699@gmail.com>
2019-10-18 01:52:44 +0700

    ต่อ T_Order กับ BackEnd

--- src/Trader/T_Order.js
+++ src/Trader/T_Order.js
@@ -1,6 +1,44 @@
 import React, { Component } from 'react'
+import {user_token} from '../Support/Constance'
+import {get,post} from '../Support/Service'
 
 class T_Order extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            product_name:'',
+            nutrient:''
+        }
+    }
+
+    handleChange = (e) =>{
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    send_demand = async (status) =>{
+        let object = {
+            product_name: this.state.product_name,
+            nutrient: this.state.nutrient,
+            product_status:status
+        }
+        try{
+            await post(object,'trader/add_send_demand',user_token).then((result)=>{
+                if(result.success){
+                    alert('เรียบร้อย')
+                }
+                else{
+                    alert(result.error_message)
+                }
+            })
+
+        }
+        catch(error){
+            alert('send_demand: '+error)
+        }
+    }
+
     render() {
         return (
             <div className="App">
@@ -14,14 +52,14 @@ class T_Order extends Component {
                     <div className="col-2"></div>
                     <div className="col-8">
                         <div>
-                            <h5>ชื่อโครงการพัฒนา <input type="text" style={{width:"50%"}}/></h5>
+                            <h5>ชื่อโครงการพัฒนา <input id='product_name' onChange={this.handleChange} type="text" style={{width:"50%"}}/></h5>
                             <h5 style={{marginBottom:"0"}}>รายละเอียด</h5>
-                        <textarea name="message" rows="10" style={{ width: "100%" }}></textarea>
+                        <textarea name="message" rows="10" id='nutrient' onChange={this.handleChange} style={{ width: "100%" }}></textarea>
                         </div>
                         <div>
                             <button className="BTN_Cencle">ยกเลิก</button>
-                            <button className="BTN_Signup" style={{marginTop:"0"}}>ส่งความต้องการพัฒนา</button>
-                            <button className="BTN_Signin" style={{marginTop:"0"}}>บันทึกเป็นฉบับร่าง</button>
+                            <button className="BTN_Signup" style={{marginTop:"0"}} onClick={()=>this.send_demand(1)}>ส่งความต้องการพัฒนา</button>
+                            <button className="BTN_Signin" style={{marginTop:"0"}} onClick={()=>this.send_demand(0)}>บันทึกเป็นฉบับร่าง</button>
                             
                         </div>
 
