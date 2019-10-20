d8d1e4287b117ad20abee567cc25fddfedfedc13
bas <bas10699@gmail.com>
2019-10-21 00:09:48 +0700

    GG

--- src/SE-Sub/S_Certified.js
+++ src/SE-Sub/S_Certified.js
@@ -6,13 +6,16 @@ import moment from 'moment'
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import arrow from '../Image/up-arrow.png'
+import Pagination from "../Support/Pagination";
 
 class S_Certified extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            currentPage: 1,
+            todosPerPage: 10,
             get_farmer: [],
-            click:false,
+            click: false,
         }
     }
 
@@ -62,6 +65,7 @@ class S_Certified extends Component {
             let chemical = parseInt(moment().utc(7).add('years', 543).diff(moment(element.chemical_date), 'year', true))
 
             switch (num) {
+                case 0:
                 case 1:
                 case 2:
                 case 3:
@@ -90,8 +94,19 @@ class S_Certified extends Component {
         });
 
     }
+    changeCurrentPage = numPage => {
+        this.setState({ currentPage: numPage });
+        //fetch a data
+        //or update a query to get data
+    };
 
     render() {
+        const { get_farmer, currentPage, todosPerPage } = this.state;
+        // Logic for displaying todos
+        const indexOfLastTodo = currentPage * todosPerPage;
+        const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+        const currentTodos = get_farmer.slice(indexOfFirstTodo, indexOfLastTodo);
+
         const options = {
             chart: {
                 renderTo: 'container',
@@ -118,11 +133,12 @@ class S_Certified extends Component {
                 }
             },
             xAxis: {
-                categories: ['1 ปี', '2 ปี', '3 ปี', 'มากกว่า 3 ปี']
+                categories: ['น้อยกว่า 1 ปี', '1 ปี', '2 ปี', '3 ปี', 'มากกว่า 3 ปี']
             },
             series: [{
                 name: 'Jane',
                 data: [
+                    { name: 'น้อยกว่า 1 ปี', y: 28 },
                     { name: '1 ปี', y: 338 },
                     { name: '2 ปี', y: 299 },
                     { name: '3 ปี', y: 400 },
@@ -144,6 +160,7 @@ class S_Certified extends Component {
                     </div>
                     <div className="col-4">
                         <h4 style={{ cursor: 'pointer' }} onClick={() => this.setState({ get_farmer: this.state.get_origin })}>เกษตรทั้งหมด จำนวน 1347 คน</h4>
+                        <h4 style={{ cursor: 'pointer' }} onClick={() => this.filter_chemical_date(0)}>ไม่ได้ใช้สารเคมีน้อยกว่า 1 ปี จำนวน 140 คน</h4>
                         <h4 style={{ cursor: 'pointer' }} onClick={() => this.filter_chemical_date(1)}>ไม่ได้ใช้สารเคมี 1 ปี จำนวน 338 คน</h4>
                         <h4 style={{ cursor: 'pointer' }} onClick={() => this.filter_chemical_date(2)}>ไม่ได้ใช้สารเคมี 2 ปี จำนวน 299 คน</h4>
                         <h4 style={{ cursor: 'pointer' }} onClick={() => this.filter_chemical_date(3)}>ไม่ได้ใช้สารเคมี 3 ปี จำนวน 400 คน</h4>
@@ -164,7 +181,7 @@ class S_Certified extends Component {
                 <div className="Row">
                     <div className="col-1"></div>
                     <div className="col-10">
-                        <h4 style={{textAlign:"center"}}>รายชื่อเกษตร</h4>
+                        <h4 style={{ textAlign: "center" }}>รายชื่อเกษตร</h4>
                         <table>
                             <tr>
                                 <th>ลำดับ</th>
@@ -179,7 +196,7 @@ class S_Certified extends Component {
                                 <th>พื้นที่เพาะปลูกทีได้รับการรับรองมาตรฐาน</th>
                             </tr>
 
-                            {this.state.get_farmer.map((element, index) => {
+                            {currentTodos.map((element, index) => {
                                 let diff = moment().utc(7).add('years', 543).diff(moment(element.chemical_date), 'year', true)
                                 console.log(diff)
                                 return (
@@ -194,6 +211,14 @@ class S_Certified extends Component {
                                 )
                             })}
                         </table>
+                        <div style={{ textAlign: 'center' }}>
+                            <Pagination
+                                currentPage={currentPage}
+                                totalPages={Math.ceil(get_farmer.length / todosPerPage)}
+                                changeCurrentPage={this.changeCurrentPage}
+                                theme="square-fill"
+                            />
+                        </div>
                     </div>
                 </div>
 
