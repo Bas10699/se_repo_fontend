a532bc8758ddb939067ec435d5dec5ef37772199
PEDPIIP <pedpii69@gmail.com>
2019-11-01 15:15:48 +0700

    Auto stash before merge of "master" and "origin/master"

--- src/Admin/Home.js
+++ src/Admin/Home.js
@@ -3,11 +3,6 @@ import React, { Component } from 'react';
 import { user_token } from '../Support/Constance';
 import { ip, get } from '../Support/Service';
 import { NavLink } from 'react-router-dom'
-import Product_Research from '../Researcher/Product_Research'
-import Product from './Product'
-import S_Plants_in_network from '../SE-Sub/S_Plants_in_network'
-import M_Plan from '../SE-Middle/M_Plan'
-
 
 class Home extends Component {
     constructor(props) {
@@ -15,55 +10,26 @@ class Home extends Component {
         this.state = {
             product_data: [],
             product_random:[],
-            get_user:[],
             search_order: [],
             default_image: 'https://www.lamonde.com/pub/media/catalog/product/placeholder/default/Lamonde_-_Image_-_No_Product_Image_4.png'
         }
     }
 
     componentWillMount() {
-        this.get_user()
+        this.get_product()
     }
 
-    render_page = (type_user) =>{
-        let return_page
-        switch (type_user) {
-            case "1": return_page = <div><Product_Research /></div>
-                
-                break;
-                case "2": return_page = <div><Product /></div>
-                
-                break;
-                case "3": return_page = <div><S_Plants_in_network /></div>
-                
-                break;
-                case "4": return_page = <div><M_Plan /></div>
-                
-                break;
-                case "5": return_page = <div>5</div>
-                
-                break;
-        
-            default:
-                break;
-        }
-        return return_page
-    }
-   
-    get_user = async () => {
+    get_product = async () => {
         try {
-            await get('user/get_user', user_token).then((result) => {
+            await get('trader/get_product', user_token).then((result) => {
                 if (result.success) {
                     this.setState({
-                        get_user: result.result,
-                        bank_information: result.result.bank_information
+                        product_data: result.result,
+                        search_order: result.result,
                     })
 
-                    if (result.result.type_user === "2") {
-                        this.get_demand_tarder()
-                    }
                     setTimeout(() => {
-                        console.log("get_user", result.result)
+                        console.log("get_product", result.result)
                     }, 500)
                 } else {
                     // window.location.href = "/";
@@ -71,9 +37,10 @@ class Home extends Component {
                 }
             });
         } catch (error) {
-            alert("get_user2" + error);
+            alert("get_product error" + error);
         }
     }
+
 
     render() {
 
