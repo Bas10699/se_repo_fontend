b7ff12230cb737dbd318a21a16fee5e010dcc299
PEDPIIP <pedpii69@gmail.com>
2019-11-05 18:07:44 +0700

    5/11/62 18.07

--- src/SE-Middle/M_Research/M_R_Trace.js
+++ src/SE-Middle/M_Research/M_R_Trace.js
@@ -5,6 +5,9 @@ import { element } from 'prop-types';
 import { user_token } from '../../Support/Constance';
 import { get, post } from '../../Support/Service';
 import Modal from 'react-responsive-modal';
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+
 const order = [
     {
         name_order: "ยาสมุนไพรลดความอ้วน",
@@ -40,17 +43,100 @@ class M_R_Trace extends Component {
         super(props)
         this.state = {
             open: false,
+            formula:[],
+            get_demand:[],
 
         }
     }
-    open = () => {
-        this.setState({ open: true })
+
+    componentWillMount() {
+        this.get_demand()
     }
+
+    open = (formula) => {
+        this.setState({ open: true,formula:formula })
+    }
+
     onCloseModal = () => {
         this.setState({ open: false });
 
     };
+
+    get_demand = async () => {
+        try {
+            await get('researcher/get_demand_trader_all', user_token).then((result) => {
+                if (result.success) {
+                    this.setState({
+                        get_demand: result.result
+                    })
+                    console.log("get_demand", result.result)
+                }
+                else {
+                    alert("get_demand", result.error_message)
+                }
+            })
+        }
+        catch (error) {
+            alert('get_demand' + error)
+        }
+    }
+
     render() {
+        let options = {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: 'กราฟแสดงผลอัตราส่วนสารอาหาร',
+                style: {
+                    fontSize: '20px',
+                    fontFamily: 'fc_lamoonregular'
+                }
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                    },
+                    style: {
+                        fontSize: '20px',
+                        fontFamily: 'fc_lamoonregular'
+                    }
+                }
+            },
+            credits: {
+                enabled: false
+            },
+            series: [{
+                name: 'อัตราส่วน',
+                style: {
+                    fontSize: '20px',
+                    fontFamily: 'fc_lamoonregular'
+                },
+                colorByPoint: true,
+                data: [{
+                    name: 'โปรตีน',
+                    y: 20,
+                }, {
+                    name: 'เเคลเซียม',
+                    y: 60,
+                }, {
+                    name: 'คาร์โบไฮเดรต',
+                    y: 10
+                }]
+            }]
+        };
+
+
         return (
             <div className="App">
                 <div className="Row">
@@ -81,7 +167,7 @@ class M_R_Trace extends Component {
                                         <td>{element.price}</td>
                                         <td>{element.number * element.price}</td>
                                         <td>รูป</td>
-                                        <td><button onClick={() => { this.open() }}>รายละเอียด</button></td>
+                                        <td><button onClick={() => { this.open(element.formula) }}>รายละเอียด</button></td>
                                     </tr>
                                 )
                             })}
@@ -93,13 +179,35 @@ class M_R_Trace extends Component {
                 <Modal open={this.state.open} onClose={this.onCloseModal}>
                     <div className="Row" style={{ width: "500px" }}>
                         <div className="col-11">
-                            <h3 style={{ textAlign: "center" }}>รายละเอียด</h3>
+                            <h3 style={{ textAlign: "center" }}>รายละเอียด {this.state.formula}</h3>
 
-                            <button onClick={() => this.onCloseModal()}>ยืนยัน</button>
                         </div>
                         <div className="col-1" />
                     </div>
-
+                    <div className="Row">
+                        <div className="col-12">
+                            <HighchartsReact highcharts={Highcharts} options={options} />
+                            <table style={{ textAlign: "center" }}>
+                                <tr>
+                                    <th colSpan="4">รายชื่อวัตถุดิบ</th>
+                                </tr>
+                                <tr>
+                                    <th>ชื่อพืช </th>
+                                    <th>จำนวน/หน่วย</th>
+                                    <th>ราคาเฉลี่ย</th>
+                                    <th>ราคารวมเฉลี่ย</th>
+                                </tr>
+                                <tr>
+                                    <td>ข้าวหอมมะลิ</td>
+                                    <td>95</td>
+                                    <td>3</td>
+                                    <td>285</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <h4>ย้อนกลับ หมายเลขหน้าที่แสดง ถัดไป</h4> 
+                    <button onClick={() => this.onCloseModal()} className="BTN_CONFIRM" style={{float:"right"}}>เลือกสูตรผลิตภัณฑ์นี้</button>
                 </Modal>
             </div>
         )
