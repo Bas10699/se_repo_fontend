a56f8a8c48ab88d849d0559165de67e629daf9d8
bas <bas10699@gmail.com>
2019-11-11 18:50:20 +0700

    M_R_Formula

--- src/SE-Middle/M_Research/M_R_Formula.js
+++ src/SE-Middle/M_Research/M_R_Formula.js
@@ -1,11 +1,47 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
+import { get, post, ip } from '../../Support/Service'
+import { user_token } from '../../Support/Constance'
+import queryString from 'query-string';
+
 class M_R_Formula extends Component {
-    render(){
-        return(
+    constructor(props) {
+        super(props)
+        this.state = {
+            product_plan: []
+        }
+    }
+    componentWillMount() {
+        this.get_product_plan()
+    }
+
+    get_product_plan = async () => {
+        let url = this.props.location.search;
+        let params = queryString.parse(url);
+        try {
+            await post(params, 'neutrally/get_product_plan', user_token).then((result) => {
+                if (result.success) {
+                    this.setState({
+                        product_plan: result.result
+                    })
+                    console.log(result.result)
+                }
+                else {
+                    alert(result.error_message)
+                    window.location.href = "/M_Demand/M_R_Trace"
+                }
+            })
+        }
+        catch (error) {
+            alert(error)
+        }
+    }
+
+    render() {
+        return (
             <div className="App">
                 <div className="Row">
                     <div className="col-12">
-                        <h2 style={{textAlign:"center"}}>ทำการกรองสูตรพัฒนาผลิตภัณฑ์ ชื่อผลิตภัณฑ์</h2>
+                        <h2 style={{ textAlign: "center" }}>ทำการกรองสูตรพัฒนาผลิตภัณฑ์ ชื่อผลิตภัณฑ์</h2>
                     </div>
                 </div>
 
